19f600fa84f996ba0056722c77dbe619764f4d19
GustBitencourt <gubiten@gmail.com>
2022-02-15 14:25:32 -0300

    animacao do fundo do jogo na funcao de comeco de jogo no javascript

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,26 @@
 function start() {
+  //oculta a div
+  $("#inicio").hide();
 
-    //oculta a div
-    $("#inicio").hide();
-	
-    //criando as divs na div fundo game
-	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
-	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
-	$("#fundoGame").append("<div id='inimigo2'></div>");
-	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+  //criando as divs na div fundo game
+  $("#fundoGame").append("<div id='jogador' class='anima1'></div>");
+  $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
+  $("#fundoGame").append("<div id='inimigo2'></div>");
+  $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
-} 
+  //Principais variáveis do jogo
+  var jogo = {};
+
+  //Game Loop
+  jogo.timer = setInterval(loop, 30);
+
+  function loop() {
+    movefundo();
+  }
+
+  //Função que movimenta o fundo do jogo
+  function movefundo() {
+    esquerda = parseInt($("#fundoGame").css("background-position"));
+    $("#fundoGame").css("background-position", esquerda - 1);
+  } 
+}
