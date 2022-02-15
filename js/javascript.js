9461e72795f1efe8083aa207af7707ca99913e19
GustBitencourt <gubiten@gmail.com>
2022-02-15 15:29:05 -0300

    funcao de disparo

--- js/javascript.js
+++ js/javascript.js
@@ -15,6 +15,7 @@ function start() {
     S: 83,
     D: 68,
   };
+  var podeAtirar = true;
   //referente ao inimigo
   var velocidade = 5;
   var posicaoY = parseInt(Math.random() * 334);
@@ -68,6 +69,35 @@ function start() {
 
     if (jogo.pressionou[TECLA.D]) {
       //Chama função Disparo
+      disparo();
+      function disparo() {
+        if (podeAtirar == true) {
+          podeAtirar = false;
+
+          topo = parseInt($("#jogador").css("top"));
+          posicaoX = parseInt($("#jogador").css("left"));
+          tiroX = posicaoX + 190;
+          topoTiro = topo + 37;
+          //criando div disparo
+          $("#fundoGame").append("<div id='disparo'></div");
+          $("#disparo").css("top", topoTiro);
+          $("#disparo").css("left", tiroX);
+
+          var tempoDisparo = window.setInterval(executaDisparo, 30);
+        }
+
+        function executaDisparo() {
+          posicaoX = parseInt($("#disparo").css("left"));
+          $("#disparo").css("left", posicaoX + 15);
+
+          if (posicaoX > 900) {
+            window.clearInterval(tempoDisparo);
+            tempoDisparo = null;
+            $("#disparo").remove();
+            podeAtirar = true;
+          }
+        }
+      }
     }
   }
 
