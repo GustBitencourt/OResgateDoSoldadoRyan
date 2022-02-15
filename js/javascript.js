bbeb9fe112bd29f4babda22e801554b439daf825
GustBitencourt <gubiten@gmail.com>
2022-02-15 14:22:51 -0300

    animacao helicopeteros e boneco

--- js/javascript.js
+++ js/javascript.js
@@ -4,9 +4,9 @@ function start() {
     $("#inicio").hide();
 	
     //criando as divs na div fundo game
-	$("#fundoGame").append("<div id='jogador'></div>");
-	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
+	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
-	$("#fundoGame").append("<div id='amigo'></div>");
+	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
 } 
