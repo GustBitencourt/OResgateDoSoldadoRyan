c296231d8267ae21d1e7b0febd24debda51ceaec
GustBitencourt <gubiten@gmail.com>
2022-02-15 11:30:31 -0300

    funcao js pra inico do jogo

---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+function start() {
+
+    //oculta a div
+    $("#inicio").hide();
+	
+    //criando as divs na div fundo game
+	$("#fundoGame").append("<div id='jogador'></div>");
+	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='inimigo2'></div>");
+	$("#fundoGame").append("<div id='amigo'></div>");
+
+} 
